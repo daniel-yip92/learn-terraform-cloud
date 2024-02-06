b3112eaeaa7bcf9aac8f869cf3b9f027d636c38d
Daniel Yip <danielyip@JJs-MacBook-Pro.local>
2024-02-06 15:10:03 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "Tutorial_Organisation"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
